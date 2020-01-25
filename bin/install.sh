bbb08c18df6ee34928f420c065b62ef821ba3d5c
Hide Charo (ひで) <59493482+hidecharo@users.noreply.github.com>
2020-01-25 13:36:36 +0900

    mod bin/install.sh bugfix

--- bin/install.sh
+++ bin/install.sh
@@ -109,7 +109,7 @@ function installNvm()
   mkdir -p "${NVM_DIR}"
   gitPullOrClone "https://github.com/nvm-sh/nvm.git" "${NVM_DIR}"
   cd "${NVM_DIR}"
-  git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`
+  #git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`
   \. "${NVM_DIR}/nvm.sh"
 
   # install Node.js + npm (version: LTS = Long Term Support)
